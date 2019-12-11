8cbff3194025237dfce406ec18b8ed4da960508c
ruinernin <ruinernin@mail.com>
2019-12-11 19:18:52 +0000

    Fixup after moving files

--- plugin.video.foxymeta/resources/lib/metadata.py
+++ plugin.video.foxymeta/resources/lib/metadata.py
@@ -158,7 +158,7 @@ def tmdb_movie(tmdb_id):
 
 @router.cache
 def tmdb_poster(tmdb_id, resolution='w780'):
-    movie = metadata.tmdb_movie(tmdb_id)
+    movie = tmdb_movie(tmdb_id)
     poster_path = movie['poster_path']
     return '{}/{}{}'.format(tmdb.IMAGE_URI,
                             resolution,
@@ -167,7 +167,7 @@ def tmdb_poster(tmdb_id, resolution='w780'):
 
 @router.cache
 def tmdb_backdrop(tmdb_id, resolution='original'):
-    movie = metadata.tmdb_movie(tmdb_id)
+    movie = tmdb_movie(tmdb_id)
     backdrop_path = movie['backdrop_path']
     return '{}/{}{}'.format(tmdb.IMAGE_URI,
                             resolution,
