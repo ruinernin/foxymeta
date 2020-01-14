9c03af629ff4d4f53962ebf349b8aaeb01c11749
drinfernoo <drinfernoo@yahoo.com>
2020-01-13 16:04:47 -0800

    :sparkles: - Add settings regarding Trakt sync

--- plugin.video.foxymeta/service.py
+++ plugin.video.foxymeta/service.py
@@ -9,12 +9,15 @@ from resources.lib.router import router
 def main():
     monitor = xbmc.Monitor()
     while not monitor.abortRequested():
+        if not router.addon.getSettingBool('trakt.access_token'):
+            continue
+    
         if router.addon.getSettingBool('library.sync.traktcollection.movies'):
             library.sync_movie_collection()
         if router.addon.getSettingBool('library.sync.traktcollection.tv'):
             library.sync_show_collection()
         sleep_mins = 45 + int(random.random() * 15)
-        if not monitor.waitForAbort(sleep_mins * 60):
+        if not monitor.waitForAbort(router.addon.getSettingInt('trakt.sync_time_period') * sleep_mins):
             break
 
 
