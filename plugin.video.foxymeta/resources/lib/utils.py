32e14704d76f872a8a25c247d7b11172488e79a5
drinfernoo <drinfernoo@yahoo.com>
2020-01-23 16:52:51 -0800

    :goal_net: - Catch some errors in utils

--- plugin.video.foxymeta/resources/lib/utils.py
+++ plugin.video.foxymeta/resources/lib/utils.py
@@ -1,10 +1,16 @@
+import xbmc
+
 import errno
 import os
 import re
+import sys
 import unicodedata
 
 
 def mkdir(path):
+    if os.path.exists(path):
+        return True
+    
     try:
         os.makedirs(path)
     except OSError as err:
@@ -18,7 +24,18 @@ def mkdir(path):
 
 def get_valid_filename(filename):
     """Returns normalized ASCII filename."""
-    ascii_name = unicodedata.normalize('NFKD', filename).encode('ascii',
-                                                                'ignore')
-    ascii_name = ascii_name.replace(' ', '_')
+    ascii_name = filename
+    try:
+        ascii_name = unicodedata.normalize('NFKD', filename).encode('ascii',
+                                                                    'ignore')
+    except:
+        log('{} could not be normalized'.format(filename))
+    finally:
+        ascii_name = ascii_name.replace(' ', '_')
+        
     return re.sub(r'[^\w.-]', '', ascii_name)
+    
+    
+def log(msg, level=xbmc.LOGDEBUG):
+    msg = '{}: {}'.format(sys.argv[0], msg)
+    xbmc.log(msg, level)
